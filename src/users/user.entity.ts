69491e1f0ab4f964c6bf52514fd7f6bf454a95e1
Sito8943 <sito8943@gmail.com>
2024-04-04 10:29:37 -0400

    added user entity docs

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,10 @@
 import { Entity, Column } from "typeorm";
 import { Model } from "src/models/model";
 
+/**
+ * @class User
+ * @description Represents an user
+ */
 @Entity({ name: "users" })
 export class User extends Model {
   @Column({ unique: true })
