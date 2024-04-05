100a2d795db73fe6cb3bc3217094b14cca83ab81
Sito8943 <sito8943@gmail.com>
2024-04-05 10:20:35 -0400

    province service and module

--- src/province/province.controller.ts
+++ src/province/province.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 
-@Controller('province')
-export class ProvinceController {}
+// dto
+import { ProvinceDto } from "./dto/province.dto";
+import { AddProvinceDto } from "./dto/add-province.dto";
+
+// services
+import { ProvinceService } from "./province.service";
+import { UpdateProvinceDto } from "./dto/update-province.dto";
+
+@Controller("province")
+export class ProvinceController {
+  constructor(private provinceService: ProvinceService) {}
+
+  @Get()
+  get(): Promise<ProvinceDto[]> {
+    return this.provinceService.get();
+  }
+
+  @Get(":id")
+  getById(@Param("id", ParseIntPipe) id: number) {
+    return this.provinceService.getById(id);
+  }
+
+  @Post()
+  create(@Body() newProvince: AddProvinceDto) {
+    return this.provinceService.create(newProvince);
+  }
+
+  @Delete(":id")
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.provinceService.remove(id);
+  }
+
+  @Patch(":id")
+  update(@Param("id", ParseIntPipe) id: number, @Body() data: UpdateProvinceDto) {
+    return this.update(id, data);
+  }
+}
--- src/province/province.service.ts
+++ src/province/province.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
+
+// entity
+import { Province } from "./province.entity";
+
+// dto
+import { AddProvinceDto } from "./dto/add-province.dto";
+import { UpdateProvinceDto } from "./dto/update-province.dto";
 
 @Injectable()
-export class ProvinceService {}
+export class ProvinceService {
+  constructor(@InjectRepository(Province) private provinceService: Repository<Province>) {}
+
+  async create(province: AddProvinceDto) {
+    const provinceFound = await this.provinceService.findOne({
+      where: { name: province.name },
+    });
+
+    if (provinceFound) {
+      return new HttpException("Province already exists", HttpStatus.CONFLICT);
+    }
+
+    const newProvince = this.provinceService.create(province);
+    return this.provinceService.save(newProvince);
+  }
+
+  get() {
+    return this.provinceService.find();
+  }
+
+  async getById(id: number) {
+    const provinceFound = await this.provinceService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!provinceFound) {
+      return new HttpException("Province not Found", HttpStatus.NOT_FOUND);
+    }
+
+    return provinceFound;
+  }
+
+  async remove(id: number) {
+    const result = await this.provinceService.delete({ id });
+    if (result.affected === 0) {
+      return;
+    }
+
+    return result;
+  }
+
+  async update(id: number, data: UpdateProvinceDto) {
+    const provinceFound = await this.provinceService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!provinceFound) {
+      return new HttpException("Province not Found", HttpStatus.NOT_FOUND);
+    }
+
+    const updatedProvince = Object.assign(provinceFound, data);
+
+    return this.provinceService.save(updatedProvince);
+  }
+}
