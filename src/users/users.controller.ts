c0b826b60533fabfa753fc837f905ce058340596
Sito8943 <sito8943@gmail.com>
2024-04-04 20:05:30 -0400

    created user controller crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
+
+// dto
+import { UserDto } from "./dto/user.dto";
+import { AddUserDto } from "./dto/add-user.dto";
+
+// services
+import { UsersService } from "./users.service";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Controller("users")
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  get(): Promise<UserDto[]> {
+    return this.usersService.get();
+  }
+
+  @Get(":id")
+  getById(@Param("id", ParseIntPipe) id: number): Promise<UserDto> {
+    return this.usersService.getById(id);
+  }
+
+  @Post()
+  create(@Body() newUser: AddUserDto): Promise<UserDto> {
+    return this.usersService.create(newUser);
+  }
+
+  @Delete(":id")
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
+  }
+
+  @Patch(":id")
+  update(@Param("id", ParseIntPipe) id: number, @Body() data: UpdateUserDto) {
+    return this.update(id, data);
+  }
+}
