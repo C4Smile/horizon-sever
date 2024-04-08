815db94a3f627643ed5b53843f553423be02b769
Sito8943 <sito8943@gmail.com>
2024-04-08 09:27:58 -0400

    paymentmethod controller/sercvice

--- src/paymentmethod/paymentmethod.controller.ts
+++ src/paymentmethod/paymentmethod.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 
-@Controller('paymentmethod')
-export class PaymentMethodController {}
+// dto
+import { PaymentMethodDto } from "./dto/paymentMethod.dto";
+import { AddPaymentMethodDto } from "./dto/add-paymentMethod.dto";
+
+// services
+import { PaymentMethodService } from "./paymentMethod.service";
+import { UpdatePaymentMethodDto } from "./dto/update-paymentMethod.dto";
+
+@Controller("paymentMethod")
+export class PaymentMethodController {
+  constructor(private paymentMethodService: PaymentMethodService) {}
+
+  @Get()
+  get(): Promise<PaymentMethodDto[]> {
+    return this.paymentMethodService.get();
+  }
+
+  @Get(":id")
+  getById(@Param("id", ParseIntPipe) id: number) {
+    return this.paymentMethodService.getById(id);
+  }
+
+  @Post()
+  create(@Body() newPaymentMethod: AddPaymentMethodDto) {
+    return this.paymentMethodService.create(newPaymentMethod);
+  }
+
+  @Delete(":id")
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.paymentMethodService.remove(id);
+  }
+
+  @Patch(":id")
+  update(@Param("id", ParseIntPipe) id: number, @Body() data: UpdatePaymentMethodDto) {
+    return this.update(id, data);
+  }
+}
--- src/paymentmethod/paymentmethod.service.ts
+++ src/paymentmethod/paymentmethod.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
+
+// entity
+import { PaymentMethod } from "./paymentMethod.entity";
+
+// dto
+import { AddPaymentMethodDto } from "./dto/add-paymentMethod.dto";
+import { UpdatePaymentMethodDto } from "./dto/update-paymentMethod.dto";
 
 @Injectable()
-export class PaymentMethodService {}
+export class PaymentMethodService {
+  constructor(
+    @InjectRepository(PaymentMethod) private paymentMethodService: Repository<PaymentMethod>,
+  ) {}
+
+  async create(paymentMethod: AddPaymentMethodDto) {
+    const paymentMethodFound = await this.paymentMethodService.findOne({
+      where: { name: paymentMethod.name },
+    });
+
+    if (paymentMethodFound) {
+      return new HttpException("PaymentMethod already exists", HttpStatus.CONFLICT);
+    }
+
+    const newPaymentMethod = this.paymentMethodService.create(paymentMethod);
+    return this.paymentMethodService.save(newPaymentMethod);
+  }
+
+  get() {
+    return this.paymentMethodService.find();
+  }
+
+  async getById(id: number) {
+    const paymentMethodFound = await this.paymentMethodService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!paymentMethodFound) {
+      return new HttpException("PaymentMethod not Found", HttpStatus.NOT_FOUND);
+    }
+
+    return paymentMethodFound;
+  }
+
+  async remove(id: number) {
+    const result = await this.paymentMethodService.delete({ id });
+    if (result.affected === 0) {
+      return;
+    }
+
+    return result;
+  }
+
+  async update(id: number, data: UpdatePaymentMethodDto) {
+    const paymentMethodFound = await this.paymentMethodService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!paymentMethodFound) {
+      return new HttpException("PaymentMethod not Found", HttpStatus.NOT_FOUND);
+    }
+
+    const updatedPaymentMethod = Object.assign(paymentMethodFound, data);
+
+    return this.paymentMethodService.save(updatedPaymentMethod);
+  }
+}
