36df5b71e0039ef63617a74b938f90b76ffdd609
Sito8943 <sito8943@gmail.com>
2024-04-08 09:26:38 -0400

    currency controller / service

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 
-@Controller('currency')
-export class CurrencyController {}
+// dto
+import { CurrencyDto } from "./dto/currency.dto";
+import { AddCurrencyDto } from "./dto/add-currency.dto";
+
+// services
+import { CurrencyService } from "./currency.service";
+import { UpdateCurrencyDto } from "./dto/update-currency.dto";
+
+@Controller("currency")
+export class CurrencyController {
+  constructor(private currencyService: CurrencyService) {}
+
+  @Get()
+  get(): Promise<CurrencyDto[]> {
+    return this.currencyService.get();
+  }
+
+  @Get(":id")
+  getById(@Param("id", ParseIntPipe) id: number) {
+    return this.currencyService.getById(id);
+  }
+
+  @Post()
+  create(@Body() newCurrency: AddCurrencyDto) {
+    return this.currencyService.create(newCurrency);
+  }
+
+  @Delete(":id")
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.currencyService.remove(id);
+  }
+
+  @Patch(":id")
+  update(@Param("id", ParseIntPipe) id: number, @Body() data: UpdateCurrencyDto) {
+    return this.update(id, data);
+  }
+}
--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
+
+// entity
+import { Currency } from "./currency.entity";
+
+// dto
+import { AddCurrencyDto } from "./dto/add-currency.dto";
+import { UpdateCurrencyDto } from "./dto/update-currency.dto";
 
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+  constructor(@InjectRepository(Currency) private currencyService: Repository<Currency>) {}
+
+  async create(currency: AddCurrencyDto) {
+    const currencyFound = await this.currencyService.findOne({
+      where: { name: currency.name },
+    });
+
+    if (currencyFound) {
+      return new HttpException("Currency already exists", HttpStatus.CONFLICT);
+    }
+
+    const newCurrency = this.currencyService.create(currency);
+    return this.currencyService.save(newCurrency);
+  }
+
+  get() {
+    return this.currencyService.find();
+  }
+
+  async getById(id: number) {
+    const currencyFound = await this.currencyService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!currencyFound) {
+      return new HttpException("Currency not Found", HttpStatus.NOT_FOUND);
+    }
+
+    return currencyFound;
+  }
+
+  async remove(id: number) {
+    const result = await this.currencyService.delete({ id });
+    if (result.affected === 0) {
+      return;
+    }
+
+    return result;
+  }
+
+  async update(id: number, data: UpdateCurrencyDto) {
+    const currencyFound = await this.currencyService.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!currencyFound) {
+      return new HttpException("Currency not Found", HttpStatus.NOT_FOUND);
+    }
+
+    const updatedCurrency = Object.assign(currencyFound, data);
+
+    return this.currencyService.save(updatedCurrency);
+  }
+}
