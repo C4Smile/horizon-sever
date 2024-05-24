1f7d42b62591efc77d9995f9c93c5c0b5f062e9f
Sito8943 <sito8943@gmail.com>
2024-05-24 10:08:03 -0400

    added news add dto

--- src/news/dto/add-news.dto.ts
+++ src/news/dto/add-news.dto.ts
@@ -3,4 +3,8 @@ import { AddModelDto } from "src/models/add-model.dto";
 
 export interface AddNewsDto extends AddModelDto {
   title: string;
+  description: string;
+  provinceId: number;
+  photoId: number;
+  tagsId: number[];
 }
