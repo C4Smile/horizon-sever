2bc2e0f6e9c05f831e06182696afc3875d378237
Sito8943 <sito8943@gmail.com>
2024-07-08 10:23:27 -0400

    fixed event has image

--- src/eventHasImage/event-has-image.entity.ts
+++ src/eventHasImage/event-has-image.entity.ts
@@ -4,11 +4,11 @@ import { Entity, PrimaryColumn } from "typeorm";
  * @class EventHasImage
  * @description Represents the relationship between events and images
  */
-@Entity({ name: "events" })
+@Entity({ name: "eventHasImage" })
 export class EventHasImage {
   @PrimaryColumn()
   eventId: number = 0;
 
-  @PrimaryColumn("increment")
+  @PrimaryColumn()
   imageId: number = 0;
 }
