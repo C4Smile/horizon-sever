7eea68f1267230fe357979a5ca0e6d2a0b6de2a2
Sito8943 <sito8943@gmail.com>
2024-07-10 10:07:34 -0400

    added new props to entity Room

--- src/room/room.entity.ts
+++ src/room/room.entity.ts
@@ -1,12 +1,11 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, OneToMany } from "typeorm";
 
 // entities
 import { Model } from "src/models/model";
-
-export enum RoomStatus {
-  operational = "operational",
-  maintenance = "maintenance",
-}
+import { Photo } from "src/image/image.entity";
+import { Photo360 } from "src/image360/image-360.entity";
+import { RoomStatus } from "src/roomStatus/room-status.entity";
+import { RoomType } from "src/roomType/room-type.entity";
 
 /**
  * @class Room
@@ -30,5 +29,46 @@ export class Room extends Model {
   content: string = "";
 
   @Column({ type: "int8" })
-  status: RoomStatus = RoomStatus.operational;
+  status: number;
+
+  @Column({ type: "int8" })
+  type: number;
+
+  @OneToMany(() => RoomStatus, (roomStatus) => roomStatus.Rooms, { cascade: true })
+  roomStatus: RoomStatus;
+
+  @OneToMany(() => RoomType, (roomType) => roomType.Rooms, { cascade: true })
+  roomType: RoomType;
+
+  @ManyToMany(() => Photo, (image) => image.Rooms)
+  @JoinTable({
+    name: "room-image",
+    joinColumn: {
+      name: "roomId",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "roomHasImage",
+    },
+    inverseJoinColumn: {
+      name: "imageId",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "imageOfRoom",
+    },
+  })
+  roomHasImage: Photo[];
+
+  @ManyToMany(() => Photo360, (image360) => image360.Rooms)
+  @JoinTable({
+    name: "room-image",
+    joinColumn: {
+      name: "roomId",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "roomHasImage360",
+    },
+    inverseJoinColumn: {
+      name: "image360Id",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "image360OfRoom",
+    },
+  })
+  roomHasImage360: Photo[];
 }
