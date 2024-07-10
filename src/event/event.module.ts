17dcf123f2b85f8f8376e371816c239b0184fb44
Sito8943 <sito8943@gmail.com>
2024-07-10 08:12:47 -0400

    removed ProvinceModule from EventModule

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -8,14 +8,13 @@ import { EventController } from "./event.controller";
 import { EventService } from "./event.service";
 
 // module
-import { ProvinceModule } from "src/province/province.module";
 import { TagModule } from "src/tags/tag.module";
 
 // entities
 import { Event } from "./event.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event]), ProvinceModule, TagModule],
+  imports: [TypeOrmModule.forFeature([Event]), TagModule],
   controllers: [EventController],
   providers: [EventService],
   exports: [EventService],
