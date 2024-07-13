6616f0009d9d0eae420d01bd95abb2047539a3bd
Sito8943 <sito8943@gmail.com>
2024-07-13 13:50:33 -0400

    reduced user

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,5 @@
-import { Entity, Column, ManyToOne } from "typeorm";
+import { Entity, Column } from "typeorm";
 import { Model } from "src/models/model";
-
-// entity
-import { Photo } from "src/image/image.entity";
 
 /**
  * @class User
@@ -10,93 +7,13 @@ import { Photo } from "src/image/image.entity";
  */
 @Entity({ name: "users" })
 export class User extends Model {
-  @Column({ type: "text", unique: true })
-  username: string;
-
-  @Column({ type: "text" })
-  password: string = "";
-
-  @Column({ type: "text" })
-  name: string = "";
 
   @Column({ type: "text", unique: true })
   email: string;
 
+  @Column({ type: "text" })
+  encrypted_password: string = "";
+
   @Column({ type: "text", unique: true })
   phone: string;
-
-  @Column({ type: "text" })
-  address: string = "";
-
-  @Column({ type: "text", unique: true })
-  identification: string;
-
-  @Column({ type: "int8" })
-  photoId: number;
-
-  @ManyToOne(() => Photo, (image) => image.Users)
-  photo: Photo;
-
-  /**
-   * @returns Name
-   */
-  get Name() {
-    return this.name;
-  }
-
-  /**
-   * @returns Email
-   */
-  get Email() {
-    return this.email;
-  }
-
-  /**
-   * @returns Username
-   */
-  get Username() {
-    return this.username;
-  }
-
-  /**
-   * @returns Password
-   */
-  get Password() {
-    return this.password;
-  }
-
-  /**
-   * @returns Phone
-   */
-  get Phone() {
-    return this.phone;
-  }
-
-  /**
-   * @returns Address
-   */
-  get Address() {
-    return this.address;
-  }
-
-  /**
-   * @returns Identification
-   */
-  get Identification() {
-    return this.identification;
-  }
-
-  /**
-   * @returns PhotoId
-   */
-  get PhotoId() {
-    return this.photoId;
-  }
-
-  /**
-   * @returns Photo
-   */
-  get Photo() {
-    return this.photo;
-  }
 }
