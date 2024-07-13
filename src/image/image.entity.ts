44d5d3256f81fa773fe1c9c7061c4aa7384dd245
Sito8943 <sito8943@gmail.com>
2024-07-13 10:34:38 -0400

    added activities and user to image

--- src/image/image.entity.ts
+++ src/image/image.entity.ts
@@ -1,10 +1,12 @@
-import { Column, Entity, ManyToMany } from "typeorm";
+import { Column, Entity, ManyToMany, OneToMany } from "typeorm";
 
 // entities
 import { Model } from "src/models/model";
 import { Event } from "src/event/event.entity";
 import { News } from "src/news/news.entity";
 import { Room } from "src/room/room.entity";
+import { User } from "src/user/user.entity";
+import { Activity } from "src/activity/activity.entity";
 
 @Entity({ name: "images" })
 export class Photo extends Model {
@@ -23,6 +25,12 @@ export class Photo extends Model {
   @ManyToMany(() => Room, (room) => room.roomHasImage, { cascade: true })
   rooms: Room[];
 
+  @OneToMany(() => User, (user) => user.photo)
+  users: User[];
+
+  @OneToMany(() => Activity, (activity) => activity.image)
+  activities: Activity[];
+
   /**
    * @returns News
    */
@@ -38,9 +46,23 @@ export class Photo extends Model {
   }
 
   /**
-   * @returns Roms
+   * @returns Rooms
    */
   get Rooms() {
     return this.rooms;
+  }
+
+  /**
+   * @return Users
+   */
+  get Users() {
+    return this.users;
+  }
+
+  /**
+   * @return Activities
+   */
+  get Activities() {
+    return this.activities;
   }
 }
