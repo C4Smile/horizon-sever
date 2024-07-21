c0990f8036a361b9568dc49536c76c1d9158e2ca
Sito8943 <sito8943@gmail.com>
2024-07-21 15:50:54 -0400

    exporting has tag to module

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -3,20 +3,23 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 // controller
 import { EventController } from "./event.controller";
+import { EventHasTagController } from "./event-has-tags.controller";
 
 // service
 import { EventService } from "./event.service";
+import { EventHasTagService } from "./event-has-tag.service";
 
 // module
 import { TagModule } from "src/tags/tag.module";
 
 // entities
 import { Event } from "./event.entity";
+import { EventHasTag } from "./event-has-.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event]), TagModule],
-  controllers: [EventController],
-  providers: [EventService],
-  exports: [EventService],
+  imports: [TypeOrmModule.forFeature([Event, EventHasTag]), TagModule],
+  controllers: [EventController, EventHasTagController],
+  providers: [EventService, EventHasTagService],
+  exports: [EventService, EventHasTagService],
 })
 export class EventModule {}
