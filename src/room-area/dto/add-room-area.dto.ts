d032715cdbb4452a7dedf5f99a53818c4d783ce9
Sito8943 <sito8943@gmail.com>
2024-07-26 09:27:57 -0400

    added missing attributes to add room-area

--- src/room-area/dto/add-room-area.dto.ts
+++ src/room-area/dto/add-room-area.dto.ts
@@ -3,4 +3,7 @@ import { AddModelDto } from "src/models/add-model.dto";
 
 export interface AddRoomAreaDto extends AddModelDto {
   name: string;
+  roomId: number;
+  description: string;
+  content: string;
 }
