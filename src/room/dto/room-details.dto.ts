317d0a75c43bc888ef110a975ec6ad249492e2da
Sito8943 <sito8943@gmail.com>
2024-07-28 14:58:10 -0400

    added NextRoomDto to RoomDetailsDto

--- src/room/dto/room-details.dto.ts
+++ src/room/dto/room-details.dto.ts
@@ -1,6 +1,7 @@
 import { AutoMap } from "@automapper/classes";
 
 // dto
+import { NextRoomDto } from "./next-room.dto";
 import { ModelDto } from "src/models/model.dto";
 import { RoomStatusDto } from "src/roomStatus/dto/room-status.dto";
 import { RoomTypeDto } from "src/roomType/dto/room-type.dto";
@@ -27,8 +28,5 @@ export class RoomDetailsDto extends ModelDto {
   @AutoMap()
   type: RoomTypeDto;
 
-  // TODO
-  // areas: RoomAreaDto[]
-
-
+  nextRoom: NextRoomDto;
 }
