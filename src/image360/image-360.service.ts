5602185c35788dc252bb4216eed9bf6bcd414422
Sito8943 <sito8943@gmail.com>
2024-07-31 10:38:11 -0400

    image 360 services as image

--- src/image360/image-360.service.ts
+++ src/image360/image-360.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { writeFileSync } from "fs";
+import { existsSync, mkdirSync, rmSync, writeFileSync } from "fs";
 import { join } from "path";
 
 // entities
@@ -15,22 +15,44 @@ export class ImageService {
   constructor(@InjectRepository(Photo360) private imageService: Repository<Photo360>) {}
 
   async create(image: AddBlobDto) {
-    const { blob, fileName, ext } = image;
-    let url = "";
+    const { base64, folder, fileName } = image;
+    const parts = fileName.split(".");
+    const ext = parts.length > 1 ? parts.pop() : "";
 
-    const base64Data = blob.replace(`data:image\/${ext};base64,`, "");
+    const base64Data = base64.replace(`data:image\/${ext};base64,`, "");
 
-    writeFileSync(join(__dirname, "..", `public/assets/${fileName}`), base64Data);
+    if (!existsSync(join(__dirname, "../../", `public/images`)))
+      mkdirSync(join(__dirname, "../../", `public/images`));
+    if (!existsSync(join(__dirname, "../../", `public/images/${folder}`)))
+      mkdirSync(join(__dirname, "../../", `public/images/${folder}`));
+    writeFileSync(join(__dirname, "../../", `public/images${folder}${fileName}`), base64Data, "base64");
 
-    url = `/assets/${fileName}`;
+    const url = `${folder}${fileName}`;
 
-    const newsFound = await this.imageService.findOne({
+    const imageFound = await this.imageService.findOne({
       where: { fileName, url },
     });
 
-    if (newsFound) throw new HttpException("Image already exists", HttpStatus.CONFLICT);
+    if (imageFound) throw new HttpException("Image already exists", HttpStatus.CONFLICT);
 
-    const newNews = this.imageService.create({ url, fileName });
-    return this.imageService.save(newNews);
+    const newImage = this.imageService.create({ url, fileName });
+    return [this.imageService.save(newImage)];
+  }
+
+  async remove(id: number) {
+    const imageFound = await this.imageService.findOne({
+      where: { id },
+    });
+
+    if (!imageFound) throw new HttpException("Image not found", HttpStatus.NOT_FOUND);
+
+    try {
+      const path = join(__dirname, "..", `public${imageFound.url}`);
+      rmSync(path);
+    } catch (err) {
+      throw new HttpException("Error to delete image", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return this.imageService.delete({ id });
   }
 }
