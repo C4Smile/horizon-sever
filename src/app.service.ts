bbf55b019a03cf00dba7ce437e76504cf42a6ebe
Sito8943 <sito8943@gmail.com>
2024-08-03 17:09:09 -0400

    removed console .log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { join } from "path";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(join(__dirname, "..", "public"));
     return "Hello World!";
   }
 }
