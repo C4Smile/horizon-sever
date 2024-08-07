3fca0c54f2cd4406b44831f9b34bd0d41e7c3d8c
Sito8943 <sito8943@gmail.com>
2024-08-07 08:51:21 -0400

    created base crud service

---
+++ src/models/service/CrudService.ts
@@ -0,0 +1,82 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectMapper } from "@automapper/nestjs";
+import { Mapper } from "@automapper/core";
+
+import { Repository } from "typeorm";
+
+// entity
+import { Model } from "../model";
+
+// dto
+import { AddModelDto } from "../dto/add-model.dto";
+import { UpdateModelDto } from "../dto/update-model.dto";
+
+@Injectable()
+export class CrudService<
+  Entity extends Model,
+  BaseDto extends Model,
+  AddDto extends AddModelDto,
+  UpdateDto extends UpdateModelDto,
+> {
+  protected entityService: Repository<Entity>;
+  protected mapper: Mapper;
+
+  constructor(entityService: Repository<Entity>, @InjectMapper() mapper: Mapper) {
+    this.entityService = entityService;
+    this.mapper = mapper;
+  }
+
+  async create(entity: AddDto) {
+    const newEntity = this.entityService.create(entity as any);
+    const saved = await this.entityService.save(newEntity);
+    return [saved];
+  }
+
+  async get({ sort, order, page, count }) {
+    const list = await this.entityService.find({
+      skip: page * count,
+      take: (page + 1) * count,
+      order: {
+        [sort]: order,
+      },
+    });
+
+    return list;
+  }
+
+  async getById(id: number): Promise<BaseDto[]> {
+    const entityFound = await this.entityService.findOne({
+      where: {
+        id,
+      } as any,
+    });
+
+    if (!entityFound) throw new HttpException("Entity not Found", HttpStatus.NOT_FOUND);
+
+    return this.mapper.mapArrayAsync(
+      [entityFound],
+      Object as unknown as new () => Entity,
+      Object as unknown as new () => BaseDto,
+    );
+  }
+
+  async remove(ids: number[]) {
+    const result = await this.entityService.update(ids, { deleted: true } as any);
+    if (result.affected === 0) throw new HttpException("Entity not Found", HttpStatus.NOT_FOUND);
+    return result;
+  }
+
+  async update(id: number, data: UpdateDto) {
+    const entityFound = await this.entityService.findOne({
+      where: {
+        id,
+      } as any,
+    });
+
+    if (!entityFound) throw new HttpException("Entity not Found", HttpStatus.NOT_FOUND);
+
+    const updatedEntity = Object.assign(entityFound, data);
+    const saved = await this.entityService.save(updatedEntity);
+    return [saved];
+  }
+}
