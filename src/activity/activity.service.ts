761a7fd8dd891294d537db34c741a625011bc87b
Sito8943 <sito8943@gmail.com>
2024-08-10 22:55:27 -0400

    created method for mappedGet

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -10,7 +10,11 @@ import { CrudService } from "src/models/service/CrudService";
 // entity
 import { Activity } from "./activity.entity";
 
+// utils
+import { PagedResult, QueryFilter } from "src/models/types";
+
 // dto
+import { ActivityDto } from "./dto/activity.dto";
 import { AddActivityDto } from "./dto/add-activity.dto";
 import { UpdateActivityDto } from "./dto/update-activity.dto";
 
@@ -23,4 +27,13 @@ export class ActivityService extends CrudService<Activity, AddActivityDto, Updat
     const relationships = ["image"];
     super(activityService, mapper, relationships);
   }
+
+  mappedGet = async (query: QueryFilter): Promise<PagedResult<ActivityDto>> => {
+    const result = await this.get(query);
+    const mappedItems = await this.mapper.mapArrayAsync(result.items, Activity, ActivityDto);
+    return {
+      items: mappedItems,
+      total: result.total,
+    };
+  };
 }
