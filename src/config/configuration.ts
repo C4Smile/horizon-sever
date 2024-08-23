3b4c77ef9c13de63f981eddccd2040ec484af2df
Sito8943 <sito8943@gmail.com>
2024-08-23 09:58:20 -0400

    using configuration  ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,11 +4,31 @@ import { join } from "path";
 
 const YAML_CONFIG_FILENAME = "config.yaml";
 
-const config = () => {
+type DatabaseConfigurationType = {
+  url: string;
+  port: number;
+  name: string;
+  user: string;
+  password: string;
+};
+
+type ChatbotConfigurationType = {
+  api: string;
+  token: string;
+};
+
+type ConfigurationType = {
+  db: DatabaseConfigurationType;
+  chatbot: ChatbotConfigurationType;
+};
+
+const configuration = () => {
   return yaml.load(readFileSync(join(__dirname, "../../", YAML_CONFIG_FILENAME), "utf8")) as Record<
     string,
     any
-  >;
+  > as ConfigurationType;
 };
+
+const config = configuration();
 
 export default config;
