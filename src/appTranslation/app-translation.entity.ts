730536aa76315b21396bebc09ac0f42d209e5d7b
Sito8943 <sito8943@gmail.com>
2024-09-11 09:14:37 -0400

    updated no content on app translation

--- src/appTranslation/app-translation.entity.ts
+++ src/appTranslation/app-translation.entity.ts
@@ -1,9 +1,10 @@
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from "typeorm";
 import { AutoMap } from "@automapper/classes";
 
 // entities
 import { Model } from "src/models/model";
 import { App } from "src/app/app.entity";
+import { Lang } from "src/lang/lang.entity";
 
 /**
  * @class App
@@ -15,14 +16,26 @@ export class AppTranslation extends Model {
   @Column({ type: "text", unique: true })
   name: string = "";
 
-  @AutoMap()
-  @Column({ type: "text" })
-  content: string = "";
-
   @Column({ type: "int" })
   appId: number;
 
   @AutoMap()
   @ManyToOne(() => App, (app) => app.translations, { cascade: true })
   app: App;
+
+  @ManyToMany(() => Lang, (lang) => lang.translations)
+  @JoinTable({
+    name: "lang-translations",
+    joinColumn: {
+      name: "translationId",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "translationLang",
+    },
+    inverseJoinColumn: {
+      name: "langId",
+      referencedColumnName: "id",
+      foreignKeyConstraintName: "langTranslation",
+    },
+  })
+  langTranslations: Lang[];
 }
