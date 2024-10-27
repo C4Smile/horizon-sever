e0cf84b75ff3949d4b3a9525a7f0ec4a62b68dbc
Sito8943 <sito8943@gmail.com>
2024-10-26 21:42:34 -0400

    fixed scripts

--- scripts/scripts.sql
+++ scripts/scripts.sql
@@ -4,18 +4,15 @@ INSERT INTO `images` (`dateOfCreation`, `lastUpdate`, `deleted`, `fileName`, `ur
 
 -- USERS
 INSERT INTO `users` (`dateOfCreation`, `lastUpdate`, `deleted`, `email`, `encrypted_password`, `phone`) VALUES
-('2024-08-26 09:12:02', '2024-08-26 09:12:02', 0, 'bot@email.com', '$2b$10$MPHks/bwdSic91xZta3tfeGWpEF6mxslv/YLoOXTrbRdYai6wIQMO', 'bot'),
 ('2024-07-19 09:01:22', '2024-07-19 09:01:22', 0, 'administrador@email.com', '$2b$10$MPHks/bwdSic91xZta3tfeGWpEF6mxslv/YLoOXTrbRdYai6wIQMO', 'administrador'),
-('2024-07-19 09:01:22', '2024-07-19 09:01:22', 0, 'player@email.com', '$2b$10$MPHks/bwdSic91xZta3tfeGWpEF6mxslv/YLoOXTrbRdYai6wIQMO', 'player'),
-('2024-08-26 09:11:04', '2024-08-26 09:11:04', 0, 'invitado@email.com', '', 'invitado');
+('2024-07-19 09:01:22', '2024-07-19 09:01:22', 0, 'player@email.com', '$2b$10$MPHks/bwdSic91xZta3tfeGWpEF6mxslv/YLoOXTrbRdYai6wIQMO', 'player');
 
 -- HORIZON ROLE
-INSERT INTO `horizon-role` (`id`, `dateOfCreation`, `lastUpdate`, `deleted`, `name`) VALUES
-(1, '2024-07-19 09:01:36', '2024-07-19 09:01:36', 0, 'Administrador'),
-(1, '2024-07-19 09:01:36', '2024-07-19 09:01:36', 0, 'Player'),
-(3, '2024-08-26 09:18:35', '2024-08-26 09:18:35', 0, 'Invitado');
+INSERT INTO `horizon-role` (`dateOfCreation`, `lastUpdate`, `deleted`, `name`) VALUES
+('2024-07-19 09:01:36', '2024-07-19 09:01:36', 0, 'Administrador'),
+('2024-07-19 09:01:36', '2024-07-19 09:01:36', 0, 'Player');
 
 -- HORIZON USER
 INSERT INTO `horizon-user` (`dateOfCreation`, `lastUpdate`, `deleted`, `name`, `username`, `address`, `identification`, `phone`, `email`, `roleId`, `userId`, `imageId`) VALUES
-('2024-08-12 10:34:55', '2024-08-13 09:37:16', 0, 'Administrador', 'administrador', 'administrador', 'administrador', 'administrador', 'aministrador@email.com', 1, 2, 1),
+('2024-08-12 10:34:55', '2024-08-13 09:37:16', 0, 'Administrador', 'administrador', 'administrador', 'administrador', 'administrador', 'aministrador@email.com', 1, 1, 1),
 ('2024-08-12 10:34:55', '2024-08-13 09:37:16', 0, 'player', 'player', 'player', 'player', 'player', 'player@email.com', 1, 2, 1),
