5da1d67fec1d45d8f8e9e2e5ad7b7b0fed7af096
Sito8943 <sito8943@gmail.com>
2024-10-26 23:35:23 -0400

    added techs on tech type

--- src/modules/techType/entities/tech-type.entity.ts
+++ src/modules/techType/entities/tech-type.entity.ts
@@ -2,10 +2,13 @@ import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
 
 // entities
 import { Model } from "src/modules/models/model";
-
+import { Tech } from "src/modules/tech/entities/tech.entity";
 
 @Entity({ name: "tech-types" })
 export class TechType extends Model {
   @Column({ type: "text" })
   name: string = "";
+
+  @ManyToOne(() => Tech, (tech) => tech.type)
+  techs: Tech[];
 }
