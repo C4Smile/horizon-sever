abd5f751a06e585e1c923776ab3e29e9ec3391a5
Sito <sito8943@gmail.com>
2024-11-30 09:06:09 -0500

    fixed type Type

--- src/modules/tech/dto/add-tech.dto.ts
+++ src/modules/tech/dto/add-tech.dto.ts
@@ -1,8 +1,9 @@
 import { AddModelDto } from "src/modules/models/dto/add-model.dto";
+import { TechTypeDto } from "src/modules/techType/dto/tech-type.dto";
 
 export interface AddTechDto extends AddModelDto {
   name: string;
-  typeId: number;
+  type: TechTypeDto;
   imageId: number;
   description: string;
   creationTime: number;
